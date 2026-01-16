e5c10f7b68c55e6f9864b424c893f784879add67
Bruce Wang <brucew.boyang@gmail.com>
2026-01-15 21:16:27 -0500

    real sign in page

--- web/src/app/signup/page.tsx
+++ web/src/app/signup/page.tsx
@@ -27,7 +27,7 @@ export default function SignupPage() {
     }
 
      try {
-    const res = await fetch("http://localhost:3300/users/login", {
+    const res = await fetch("http://localhost:3300/users/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
