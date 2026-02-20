743db7fcdfbc169ac9b8e78d4ec0208ee5dffc15
Bruce Wang <brucew.boyang@gmail.com>
2026-02-20 04:52:03 -0500

    finish file upload feature

--- backend/auth.js
+++
@@ -1,8 +0,0 @@
-function requireAuth(req, res, next) {
-  if (!req.session || !req.session.authenticated) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-  next();
-}
-
-module.exports = requireAuth;
